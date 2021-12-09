ed62028ce90c12b2ae016aece22257ceb7898beb
Nagasaikiller <95856949+Nagasaikiller@users.noreply.github.com>
2021-12-09 17:20:27 +0530

    Add files via upload

---
+++ intrest.c
@@ -0,0 +1,11 @@
+// thursday's topic
+#include<stdio.h>
+main()
+{
+	int p,t;
+	float r,result;
+	printf("to find simple intrest");
+	scanf("%d%d%f",&p,&t,&r);
+	result=(p*t*r)/100;
+	printf("%.2f",result);
+}
