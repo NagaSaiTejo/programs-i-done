86dac184d0c1252f3999a722366ee102178ac349
Nagasaikiller <95856949+Nagasaikiller@users.noreply.github.com>
2021-12-10 22:24:26 +0530

    Add files via upload

---
+++ self info.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+	printf(" name: T.nagasai\n");
+	printf("age:15\n");
+		printf("studing: ece\n");
+			printf("birthday: 18-08-2006\n");
+				printf("father name: papayya\n");
+					printf("occupation: administrative officer\n");
+						printf("mother name: durga bhavani\n");
+							printf("occupation: lovely home maker\n");
+								printf("10th grade point: 10");
+									printf("polycet rank: 15531");
+										printf("hobbies: playing games");
+											printf("goal:getting job in campus interview");
+}
